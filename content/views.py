3bbfe8b55d1567e9102204851d56ff0820b84340
GameExpepe <156389923+GameExpepe@users.noreply.github.com>
2025-08-22 20:46:09 +0900

    Feed side complete

--- content/views.py
+++ content/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
 
 # Create your views here.
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all()
+        print(feed_list)
+
+        return render(request, "Instagram/main.html")
