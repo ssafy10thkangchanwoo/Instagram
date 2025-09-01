17dd291d6ab688c8dbf463aab480d892e05af0a6
GameExpepe <156389923+GameExpepe@users.noreply.github.com>
2025-09-01 22:30:53 +0900

    Make User model, but problem regarding atttribute/USER_FIELD

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractBaseUser):
+    profile_image = models.TextField()
+    nickname = models.CharField(max_length=24)
+    name = models.CharField(max_length=24)
+    email = models.EmailField()
+    # USERNAME_FIELD = 'email'
+
+    class Meta:
+        db_table = 'User'
