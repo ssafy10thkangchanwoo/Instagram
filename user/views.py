e8733c5db05b12a032197686635251af489ad05e
GameExpepe <156389923+GameExpepe@users.noreply.github.com>
2025-09-07 00:49:04 +0900

    create user API

--- user/views.py
+++ user/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import User
+from django.contrib.auth.hashers import make_password
 
 
 # Create your views here.
@@ -7,6 +10,26 @@ class Join(APIView):
     def get(self, request):
         return render(request, "user/join.html")
 
+    def post(self, request):
+        request.data.get("email")
+
+        email = request.data.get("email", None)
+        nickname = request.data.get("nickname", None)
+        name = request.data.get("name", None)
+        password = request.data.get("password", None)
+
+        User.objects.create(email=email,
+                            nickname=nickname,
+                            name=name,
+                            password=make_password(password),
+                            profile_image='default.jpg'
+                            )
+
+        return Response(status=200)
+
 class Login(APIView):
     def get(self, request):
         return render(request, "user/login.html")
+
+    def post(self, request):
+        pass
